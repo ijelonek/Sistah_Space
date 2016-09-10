6385442e24da69ff18dfa50554e1a6feb7d622ff
ijelonek <ijelonek@live.co.uk>
2016-09-10 23:58:32 +0100

    contact form added

---
+++ js/contact.js
@@ -0,0 +1,35 @@
+(function() {
+    function onEscapeClick() {
+      document.write('');
+      window.location = "http://youtube.co.uk"
+    }
+
+    document.getElementById('escape').addEventListener('click', onEscapeClick)
+  }())
+
+$("#contactForm").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
+});
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+ 
+    $.ajax({
+        type: "POST",
+        url: "../php/process.php",
+        data: "name=" + name + "&email=" + email + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
+    });
+}
+function formSuccess(){
+    $( "#msgSubmit" ).removeClass( "hidden" );
+}
